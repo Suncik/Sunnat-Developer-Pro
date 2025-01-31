b880cf5b49f2358c134a30ce0a348cf4a293b67f
Suncik <suncik@example.com>
2025-01-31 14:39:10 +0500

    Birinchi commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ismingizni kiriting:"); // Xabar chiqariladi
+        string name = Console.ReadLine(); // Terminaldan ma'lumot kiritiladi
+        Console.WriteLine("Salom, " + name + "!");
+    }
+}
